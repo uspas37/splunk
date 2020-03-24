81c0c51cec739deee7d559e6c79911d445711712
uspas37 <62074282+uspas37@users.noreply.github.com>
2020-03-24 17:49:53 -0400

    added success criteria to script

--- 101.sh
+++ 101.sh
@@ -11,7 +11,7 @@ su - cc3adm -c 'stopsap r3' >> $log 2>&1
 # Success Criteria 3 ways to check:
 # 1. su - cc3adm -c 'stopsap check CC3' | grep "is not running" > /dev/null
 # 2. ps -ef | grep [d]w.sapCC3 > /dev/null
-# 3. grep "stopped" $log > /dev/null
+grep "stopped" $log > /dev/null
 if [ $? -eq 0 ]
 then
   echo "Success"
